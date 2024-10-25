d778401e2357f8592c9b196c3f65e6c01db81bd6
VitaliySaburdo <vsaburdo@gmail.com>
2024-10-25 20:06:39 +0300

    fix input

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Title, TopBar } from '@/components/shared';
+import { Container, Filters, Title, TopBar } from '@/components/shared';
 
 export default function Home() {
   return (
@@ -6,6 +6,12 @@ export default function Home() {
       <Container className="mt-10">
         <Title text="All Pizzas" size="lg" className="font-extrabold" />
         <TopBar />
+        <Container className="mt-10 pb-4">
+          {/* FilterBar */}
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+        </Container>
       </Container>
     </>
   );
---
+++ components/shared/filters.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Title } from './title';
+import { FilterCheckbox } from './filter-checkbox';
+import { Input } from '../ui';
+
+interface Props {
+  className?: string;
+}
+
+export const Filters: React.FC<Props> = ({ className }) => {
+  return (
+    <div className={className}>
+      <Title text="Filtration" size="sm" className="mb-5 font-bold" />
+
+      <div className="flex flex-col gap-4">
+        <FilterCheckbox text="Can be collected" value="1" />
+        <FilterCheckbox text="New items" value="2" />
+      </div>
+      <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+        <p className="font-bold mb-3">Price from to</p>
+        <div className="flex gap-3 mb-5">
+          <Input
+            type="number"
+            placeholder="0"
+            min={0}
+            max={50}
+            defaultValue={0}
+          />
+          <Input type="number" placeholder="50" min={5} max={50} />
+        </div>
+      </div>
+    </div>
+  );
+};
