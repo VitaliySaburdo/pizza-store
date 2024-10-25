5c72a98011311761f0b6b45cf7fd1ed977004336
Vitaliy Saburdo <vsaburdo@gmail.com>
2024-10-25 11:34:31 +0300

    cart button

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,8 +1,9 @@
 import { cn } from '@/lib/utils';
 import React from 'react';
-import Container from './container';
 import Image from 'next/image';
-import { Button } from '../ui/button';
+import { ArrowRight, ShoppingCart, User } from 'lucide-react';
+import Container from './container';
+import { Button } from '../ui';
 
 interface Props {
   className?: string;
@@ -22,7 +23,21 @@ const Header: React.FC<Props> = ({ className }) => {
         </div>
         {/* Right side */}
         <div className="flex items-center gap-3">
-          <Button variant="outline">Login</Button>
+          <Button variant="outline" className="flex items-center gap-1">
+            <User size={16} />
+            Login
+          </Button>
+          <div>
+            <Button className="group relative">
+              <b>10 $</b>
+              <span className="h-full w-[1px] bg-white/30 mx-3" />
+              <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                <ShoppingCart className="h-4 w-4 relative" strokeWidth={2} />
+                <b>3</b>
+              </div>
+              <ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100" />
+            </Button>
+          </div>
         </div>
       </Container>
     </header>
