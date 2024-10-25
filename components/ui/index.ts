65132367f6c297e0f15cda35d39b139356ada0d8
VitaliySaburdo <vsaburdo@gmail.com>
2024-10-25 17:35:48 +0300

    delete export input

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -2,7 +2,6 @@ export { Button } from './button';
 export { Checkbox } from './checkbox';
 export { Dialog } from './dialog';
 export { Drawer } from './drawer';
-export { Input } from './input';
 export { Popover } from './popover';
 export { Select } from './select';
 export { Skeleton } from './skeleton';
