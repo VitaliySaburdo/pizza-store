8ab7d4a74124ce26eac0b5dd2485342e6bd17ded
Vitaliy Saburdo <vsaburdo@gmail.com>
2024-12-04 11:37:15 +0200

    page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,11 +1,3 @@
-interface ProductPageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default function ProductPage({ params }: ProductPageProps) {
-  const { id } = params;
-
-  return <p>Product {id}</p>;
+export default function ProductPage({ params }: { params: { id: string } }) {
+  return <p>Product {params.id}</p>;
 }
