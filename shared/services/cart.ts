fb148193273b9b8a281ceaae3eb56808f4275716
VitaliySaburdo <vsaburdo@gmail.com>
2025-01-21 21:14:15 +0200

    fix CartDTO

--- shared/services/cart.ts
+++ shared/services/cart.ts
@@ -1,7 +1,7 @@
+import { CartDTO } from './dto/cart.dto';
 import { axiosInstance } from './axiosInstance';
-import { Cart } from '@prisma/client';
 
-export const getCart = async (): Promise<Cart> => {
-  const { data } = await axiosInstance.get<Cart>('/cart');
+export const getCart = async (): Promise<CartDTO> => {
+  const { data } = await axiosInstance.get<CartDTO>('/cart');
   return data;
 };
